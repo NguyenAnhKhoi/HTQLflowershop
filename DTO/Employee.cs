306cdf75eb5d8801a18a7b7028fdecb431ebe04f
NguyenAnhKhoi <1651010083khoi@ou.edu.vn>
2018-11-24 09:19:53 +0700

    added Employee DTO class

---
+++ DTO/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Employee
+    {
+        public string emId { get; set; }
+        public string emName { get; set; }
+        public string accName { get; set; }
+        public string password { get; set; }
+
+        private Employee(string id, string name, string acc, string pass)
+        {
+            this.emId = id;
+            this.emName = name;
+            this.accName = acc;
+            this.password = pass;
+        }
+    }
+}
